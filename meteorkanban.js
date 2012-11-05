9fe87de6fcfde73e35bd7556a70722f9a6bc7bc7
Frode T Bjerke <frode.bjerke@gmail.com>
2012-11-05 21:29:33 +0100

    Tasks shows in the right status column

--- meteorkanban.js
+++ meteorkanban.js
@@ -1,17 +1,45 @@
+Tasks = new Meteor.Collection("tasks");
+States = new Meteor.Collection("states");
+
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to meteorkanban.";
+  Template.states.states = function () {
+    return States.find({},{sort: {position: 1}});
   };
 
-  Template.hello.events({
-    'submit form' : function (event) {
-		console.log(event);	
-	}
+  Template.addTask.text = function () {
+    return "Add a new task.";
+  };
+
+  Template.tasks.tasks = function () {
+    console.log(this);
+    return Tasks.find({position: this.position});
+  };
+
+  Template.tasks.done_checkbox = function () {
+    return this.done ? 'checked="checked"' : '';
+  };
+
+  Template.tasks.events({
+    'click .check' : function () {
+      Tasks.update(this._id, {$inc: {position: 1}});
+    }
+  });
+
+  Template.addTask.events({
+    'click .addTaskSubmit' : function () {
+      var inp = $('.addTaskName').val();
+      $('.addTaskName').val("");
+      Tasks.insert({name: inp, done: false, position: 1});
+    }
   });
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
+    if (States.find().count() == 0) {
+    	States.insert({name: "Backlog", position: 1});
+    	States.insert({name: "WIP", position: 2});
+    	States.insert({name: "Finished", position: 3});
+    }
   });
 }
