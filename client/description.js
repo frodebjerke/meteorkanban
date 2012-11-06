d714cc1169259f5c37283c2b35bf36cfc9b2b7f9
Frode T Bjerke <frode.bjerke@gmail.com>
2012-11-07 00:32:01 +0100

    Add description: edit task form. Issues/errors with persisting.

--- client/description.js
+++ client/description.js
@@ -8,9 +8,38 @@ if (Meteor.isClient) {
     return Tasks.find();
   };
 
+  Template.description.hide_edit = function () {
+    return (this.description == null || Session.equals("show_edit_desc", true) ) ? "" : "hide";
+  };
+
+  Template.description.events({
+    'click .show_edit' : function () {
+      Session.set("show_edit_desc", true);
+    }
+  });
+
+  Template.description.events({
+    'click .editSubmit' : function () {
+      var desc = $('.addDescription').val();
+      if (desc != null) {
+	Tasks.update(this._id, {$set: {description: desc}});
+      }
+      Session.set("show_edit_desc", false);
+    }
+  });
+
+  Template.description.events({
+    'click .editCancel' : function () {
+      console.log("halla");
+      $('.addDescription').val("");
+      Session.set("show_edit_desc", false);
+    }
+  });
+
   Template.description.events({
       'click .hideDesc' : function () {
 	Session.set("task_in_desc", null);
+	Session.set("show_edit_desc", false);
       }
   });
 }
