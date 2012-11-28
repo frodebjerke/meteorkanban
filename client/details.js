98a3a1e9bfe168b3a4cf71d5db0364e4ebf82e84
Frode T Bjerke <frode.bjerke@gmail.com>
2012-11-28 15:19:25 +0100

    Refactor description template into details

--- client/details.js
+++ client/details.js
@@ -1,34 +1,34 @@
 if (Meteor.isClient) {
 
-    Template.description.show = function () {
+    Template.details.show = function () {
         return Session.equals("task_in_desc", this._id) ? "" : "hide";
     };
 
-    Template.description.tasks = function () {
+    Template.details.tasks = function () {
         return Tasks.find();
     };
 
-    Template.description.hide_edit = function () {
-        return (this.description == null || Session.equals("show_edit_desc", true) ) ? "" : "hide";
+    Template.details.hide_edit = function () {
+        return (this.details == null || Session.equals("show_edit_desc", true) ) ? "" : "hide";
     };
 
-    Template.description.not_hiding_edit = function () {
-        return (this.description == null || Session.equals("show_edit_desc", true) ) ? "hide" : "";
+    Template.details.not_hiding_edit = function () {
+        return (this.details == null || Session.equals("show_edit_desc", true) ) ? "hide" : "";
     };
 
-    Template.description.events({
+    Template.details.events({
         'click .show_edit':function () {
             Session.set("show_edit_desc", true);
         }
     });
 
-    Template.description.events({
+    Template.details.events({
         'click .delete' : function () {
             Tasks.remove(this._id);
         }
     })
 
-    Template.description.events({
+    Template.details.events({
         'click .hideDesc':function () {
             Session.set("task_in_desc", null);
             Session.set("show_edit_desc", false);
